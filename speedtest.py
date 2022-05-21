55891569a8f97d92527e9d5c95201a26d2b801ed
Tumo <104463761+zachyboy12@users.noreply.github.com>
2022-05-21 18:44:45 -0400

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -1,9 +1,10 @@
 import rplus
 
-path = 'PATH TO R+ HERE'
-print('r+ (rplus) speed test:')
-print('Random word generator #1:', rplus.randomwordgen1(5, True))
-print('Random word generator #2:', rplus.randomwordgen2(True))
-print('Random number generator #1:', rplus.randomnumbergen1(path, 1, 3, return_speed=True))
-print('Random number generator #2:', rplus.randomnumbergen2(path, True))
-print('Random password generator:', rplus.randompassword(path, return_speed=True))
+def speedtest(path_to_rplus: str):
+    path = path_to_rplus
+    print('r+ (rplus) speed test:')
+    print('Random word generator #1:', rplus.randomwordgen1(5, True))
+    print('Random word generator #2:', rplus.randomwordgen2(True))
+    print('Random number generator #1:', rplus.randomnumbergen1(path, 1, 3, return_speed=True))
+    print('Random number generator #2:', rplus.randomnumbergen2(path, True))
+    print('Random password generator:', rplus.randompassword(path, return_speed=True))
